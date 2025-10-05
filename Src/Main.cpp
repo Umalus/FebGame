d5d242d7c3b661f08a7a784071e15996fe4c5485
THR_YUMA <tenderhorse67@gmail.com>
2025-10-05 23:51:27 +0900

    Initial commit

---
+++ Src/Main.cpp
@@ -0,0 +1,40 @@
+/*
+ * @project		FPS_3D_SHOOTHING_GAME
+ * @Goal		1月末までに作品提出できるくらいのレベルにする
+ */
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+int main() {
+	//glfwの初期化
+	if (!glfwInit()) return -1;
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "FPS", NULL, NULL);
+	//ウィンドウが作成されなければ処理終了
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//GLADの初期化
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "エラーやでー" << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//glfwの解放
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+
+}
