1a45a7977ab926978263699b3e9c1fc73961b33c
THR_YUMA <tenderhorse67@gmail.com>
2025-10-21 22:09:03 +0900

    シングルトンクラス実装

---
+++ Src/Singleton.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+/// <summary>
+/// シングルトンクラス
+/// 主にマネージャー系クラスが一つしか存在しないことを保証するクラス
+/// </summary>
+template <typename T>
+
+class Singleton {
+	static T& getInstace() {
+		static T instance;
+		return instance;
+	}
+
+	//コピーコンストラクタと代入演算子を削除
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+protected:		//コンストラクタとデストラクタ
+
+	/*
+	 * @brief	デフォルトコンストラクタ
+	 * @tips	特にすることなし
+	 */
+	Singleton() = default;
+
+	/*
+	 * @brief	デストラクタ
+	 * @tips	特にすることなし
+	 */
+	~Singleton() = default;
+};
